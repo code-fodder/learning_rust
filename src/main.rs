ddac91c4d46ecbb7da25e2646e84dbabe68d5dc4
adadachanji@gmail.com <adadachanji@gmail.com>
2021-05-16 21:46:18 +0100

    2hrs_21m

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,64 @@
+#![feature(doc_alias)]
 
-#[derive(Debug)]
-struct Position {
-    x: i8,
-    y: i8,
-}
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
 
-impl Position {
-    fn update_y(&mut self) {
-        self.y += 1;
+// Add the "test".rs module (mod)
+mod test;
+
+fn main() -> Result<(), String> {
+    println!("res: {}", test::do_something());
+
+    let context = sdl2::init();
+
+    // Instead of .unwrap()'ing init() above we store it in context and now we are going to deal with the two Result
+    // outcomes manually, i.e. Ok(sdl2) and Err(String). Its sdl2 and String because init returns Result<sdl2, String>.
+    let sdl_context = match context {
+        Ok(result) => result,
+        Err(message) => {
+            println!(r#"SDL init error: '{}'"#, message);
+            return Ok(()); // return from main()
+        }
+    };
+
+    // Replacing .unwrap() with '?' is roughly equivalent to the above - except the error message is not
+    // custom, its just the raw error message
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
-    fn update_x(&mut self) {
-        self.x += 1;
-    }
-
-    fn update(&mut self) {
-        self.update_y();
-        self.update_x();
-    }
-}
-
-fn main() {
-    let mut i = 1;
-    i += 1;
-    println!("i = {}", i);
-
-    let mut pos = Position {y:1, x:1};
-    pos.x = 2;
-    let pos2 = &mut pos; // effectively either pos or pos2 should take over now - can't use both, we will use pos
-    pos2.y = 2;
-    println!("pos = {:?}", pos);
-
-    pos.update();
-    println!("pos = {:?}", pos);
-    pos.update_x();
-    println!("pos = {:?}", pos);
-    pos.update_y();
-    println!("pos = {:?}", pos);
-    Position::update(&mut pos);
-    println!("pos = {:?}", pos);
-
+    Ok(()) // equivalent to: return Ok(());
 }
---
+++ src/test.rs
@@ -0,0 +1,8 @@
+#[test]
+fn some_test() -> () {
+    assert_eq!(1, 1);
+}
+
+pub fn do_something() -> i32 {
+    return 2;
+}
